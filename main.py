5fd25b492b7ecb9e5ad40f4ab10c9d4b0d139d6c
AurelienCallens <aurelien.callens@univ-pau.fr>
2022-04-14 10:32:49 +0200

    Initial commit v2

---
+++ main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 13:43:32 2022
+
+@author: aurelien
+"""
+
+import numpy as np
+from configs.Settings import *
+import matplotlib.pyplot as plt
+from model.Unet_model import UNet
+from scipy.ndimage import gaussian_filter
+from executor.tf_init import start_tf_sesssion
+from evaluation.Verif_functions import plot_history, check_nan_generator
+
+# 0) Initialize session
+mode = 'cpu'
+start_tf_sesssion(mode)
+
+# 1) Test one model
+
+Unet_model = UNet(size=img_size, bands=n_channels, model_saved=None)
+
+# Check FCN structure
+model = Unet_model.build()
+model.summary()
+
+# Check if Nan values in train gen
+# check_nan_generator(Unet_model)
+
+# Train the model
+Trained_model = Unet_model.train()
+
+# Verification of the training loss
+plot_history(test1[1], 'CNN_batch12_filter16_lr0.003_decay_0.8_40.png')
+
+preds = Unet_model.predict()
+test_gen = Unet_model.data_generator('Test')
+Trained_model[0].evaluate(test_gen)
+
+for i in np.arange(0, preds.shape[0], 3):
+    true_img = test_gen.__getitem__(i)[1]
+    pred_img = preds[i, :, :, :]
+    _vmin, _vmax = np.min([np.min(true_img), np.min(pred_img)]), np.max([np.max(true_img), np.max(pred_img)])
+    plt.subplot(1,2,1)
+    plt.imshow(true_img.squeeze().astype('float32'), cmap='jet', vmin=_vmin, vmax=_vmax)
+    plt.subplot(1,2,2)
+    plt.imshow(gaussian_filter(pred_img.squeeze().astype('float32'), sigma=8), cmap='jet', vmin=_vmin, vmax=_vmax)
+    plt.show()
+
+Trained_model[0].save('CNN_batch12_filter16_lr0.003_decay_0.8_40')
+
+
+# 3) Test several models
+batch_size = np.arange(4, 24, 4)
+filter_size = np.arange(8, 49, 8)
+combs = [(i, j) for i in batch_size for j in filter_size]
+
+for comb in combs:
+   batch_size = comb[0]
+   filters = comb[1]
+   fcn_model = UNet(size=(256, 256), bands=3, model_saved=None)
+   try:
+       temp_model = fcn_model.train()
+       test_gen = fcn_model.data_generator('Test')
+       acc = np.round(temp_model[0].evaluate(test_gen), 3)
+       name = 'FCN_acc_' + str(acc) + '_b_' + str(batch_size) + '_f_' + str(filters) + '_lr0.003_decay_0.8_40'
+       temp_model[0].save(name)
+       plot_history(temp_model[1], name + '.png')
+   except MemoryError:
+       pass
+
+
+
+
+
+
+
